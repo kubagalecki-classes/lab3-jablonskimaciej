c2a1eee5380b688f8dba86f4e3e44650d000127c
Maciej Jabłoński <73608866+jablonskimaciej@users.noreply.github.com>
2020-11-18 13:45:01 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    unsigned int licznik_towaru = 0;
+    unsigned int licznik_zaglowcow = 0;
+
+    while (towar > licznik_towaru)
+    {
+      Stocznia stocznia1{};
+      Statek* s1 = stocznia1();
+      licznik_towaru = licznik_towaru + transportuj(s1);
+      Zaglowiec* wsk_zagl = dynamic_cast< Zaglowiec* >(s1);
+      if (wsk_zagl != 0)
+        licznik_zaglowcow = licznik_zaglowcow + 1; 
+      delete s1;
+    }
     return 0;
 }
